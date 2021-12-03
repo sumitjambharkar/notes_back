efaa041cfb0f6fdda78a6b7c6d2401f778f5313a
sumitjambharkar <sujam3545@gmail.com>
2021-12-04 01:44:42 +0530

    two

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const Port = process.env.PORT || 3001
 const Note = require('./model/Notes');
+const fast2sms = require('fast-two-sms')
+const User = require('./model/User')
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
@@ -27,11 +30,32 @@ app.get('/notes',(req,res)=>{
 
 })
 
-app.get('/notes/:id',(req,res)=>{
-    const {id} = req.params
-    const iteam = Note.findOneAndDelete(id)
+app.delete('notes/:id',(req,res)=>{
+    const id = req.params.id
+    const iteam = Note.findByIdAndDelete({_id:id})
     res.json(iteam)
     
+})
+
+app.post('/user-wifi',async(req,res)=>{
+    const wifiUser = await User.create(req.body)
+    res.status(200).json(wifiUser)
+
+})
+app.post('/user-feedback',async(req,res)=>{
+    const feedBack = await User.create(req.body)
+    res.status(200).json(feedBack)
+
+})
+app.post('/user-send-message',async(req,res)=>{
+    try{
+        const user = await User.create(req.body)
+        const respone = await fast2sms.sendMessage({authorization : process.env.API_KEY , message :req.body.message, numbers : [7021595850]})
+        res.status(200).send(respone).json(user)
+    }catch(err) {
+        console.error(err);
+    }
+
 })
 
 app.listen(Port,()=>{
